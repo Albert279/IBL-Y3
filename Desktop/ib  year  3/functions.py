11e52005a58f7c2c3e98afeb3309626adeaa4721
Albert279 <alkariuki20@gmail.com>
2023-04-04 14:50:52 +0300

    This is python programming

---
+++ Desktop/ib  year  3/functions.py
@@ -0,0 +1,28 @@
+print("hello world")
+print(3.4)
+print(2)
+
+#variables
+x = 2 + 5
+y = 3
+
+print(x)
+print(y)
+print(x + y)
+print(x * y)
+
+first_number = int(input("whats your first "))
+second_number = int(input("whats your second "))
+print(first_number * second_number)
+
+number1 = int (input("whats your first "))
+number2 = int (input("whats your second "))
+final_number = number1 + number2
+print(f"{number1 * number2}")
+print("{} + {} = {}" .format(number1, number2, final_number))
+
+a= float(input("enter the first number "))
+b= float(input("enter the second number "))
+product = a* b
+print("{} * {} = {}" .format(a, b, product))
+
